138c21a000ceed9cab2dbafe202be26c280e7501
Markus Ebner <hiwatari.seiji@gmail.com>
2023-03-01 21:20:33 +0100

    Improve error handling in build script

--- make.sh
+++ make.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-pushd bin/keepEngine
-cargo build --target x86_64-unknown-linux-musl --release
-popd
-cp bin/keepEngine/target/x86_64-unknown-linux-musl/release/keepEngine res/lapas/guest/lapas/keepEngine;
+pushd bin/keepEngine;
+cargo build --target x86_64-unknown-linux-musl --release || exit $?;
+popd;
+cp bin/keepEngine/target/x86_64-unknown-linux-musl/release/keepEngine res/lapas/guest/lapas/keepEngine || exit $?;
 chmod +x res/lapas/guest/lapas/keepEngine;
 
 
-pushd bin/lapas_api_server
-cargo build --target x86_64-unknown-linux-musl --release
-popd
-cp bin/lapas_api_server/target/x86_64-unknown-linux-musl/release/lapas-api-server res/lapas/scripts/lapas-api-server;
+pushd bin/lapas_api_server;
+cargo build --target x86_64-unknown-linux-musl --release || exit $?;
+popd;
+cp bin/lapas_api_server/target/x86_64-unknown-linux-musl/release/lapas-api-server res/lapas/scripts/lapas-api-server || exit $?;
 chmod +x res/lapas/scripts/lapas-api-server;
 
 
-pushd bin/lapas_api_client
-cargo build --target x86_64-unknown-linux-musl --release
-popd
-cp bin/lapas_api_client/target/x86_64-unknown-linux-musl/release/lapas-api-client res/lapas/guest/lapas/lapas-api-client;
+pushd bin/lapas_api_client;
+cargo build --target x86_64-unknown-linux-musl --release || exit $?;
+popd;
+cp bin/lapas_api_client/target/x86_64-unknown-linux-musl/release/lapas-api-client res/lapas/guest/lapas/lapas-api-client || exit $?;
 chmod a+x res/lapas/guest/lapas/lapas-api-client;
 
 
