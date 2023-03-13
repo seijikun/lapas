ac7b010c53cb9b42172c1ed410acebeae2d43f05
Markus Ebner <hiwatari.seiji@gmail.com>
2023-03-13 18:43:42 +0100

    Add hook subsystem in /mnt/homeBase/.lapas/hooks
    This folder will contain various hooks (shell-script) that will be executed in various
    situations and environments to signal certain actions and allow customization.
    
    A (very specific) example:
    If a game brutally misbehaves and endlessly scans through the entire wineprefix filesystem,
    thus slowing down nfs to a crawl because of uncacheable requests, create an ext4 file-image
    of a certain size, put the wineprefix into it, and mount it in the homeBaseBeforeCleanup.sh
    hook to the mount-target: /mnt/homeBase/.wineManager/bottles/<game>/prefix

--- res/lapas/guest/lapas/initCleanHomeBase.sh
+++ res/lapas/guest/lapas/initCleanHomeBase.sh
@@ -2,4 +2,12 @@
 
 USER_BASE="/mnt/homeBase";
 
-"/lapas/keepEngine" base "${USER_BASE}/.keep" "$USER_BASE" || exit 1;
+sh "$USER_BASE/.lapas/hooks/homeBaseBeforeCleanup.sh" || exit $?;
+
+# When starting in user mode, this runs the cleanup process as specified by the homeBase/.keep file.
+# All users (lapas, as well as players) thus will then have a cleaned homeBase as base for their homeFolder.
+if [ -f "/.lapasUser" ]; then
+        "/lapas/keepEngine" base "${USER_BASE}/.keep" "$USER_BASE" || exit $?;
+fi
+
+sh "$USER_BASE/.lapas/hooks/homeBaseAfterCleanup.sh" || exit $?;
