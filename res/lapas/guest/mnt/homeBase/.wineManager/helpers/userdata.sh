fdde020d0f36688bde85dd0c44397a24c5bb5438
Markus Ebner <hiwatari.seiji@gmail.com>
2023-03-15 22:49:08 +0100

    Add userdata helper method for really weird cases

--- res/lapas/guest/mnt/homeBase/.wineManager/helpers/userdata.sh
+++ res/lapas/guest/mnt/homeBase/.wineManager/helpers/userdata.sh
@@ -28,6 +28,26 @@ function helperUserdataMoveLink() {
 }
 
 
+# If the destination exists, replace inputFileOrFolder with a symlink, otherwise initialize the
+# destination by moving the inputFileOrFolder to the destination, then creating a symlink
+# - inputFileOrFolder prefix relative path to the file or folder in the install folder that should be replaced by a link into userdata [e.g. drive_c/...]
+# - relativeUserDataPath userdata relative path where the file/folder should be moved to [e.g. Saved Games]
+function helperUserdataAlwaysLinkInitByMove() {
+	inputFileOrFolder="${WM_BOTTLE_PREFIX}/$1";
+	userDataPath="${WM_USERDATA_PATH}/$2";
+	targetName=$(basename "$inputFileOrFolder");
+	
+	# init by move if required
+	if [ ! -f "${userDataPath}/${targetName}" ] && [ ! -d "${userDataPath}/${targetName}" ]; then
+		mv "$inputFileOrFolder" "${userDataPath}/${targetName}" || exit 1;
+	fi
+	# remove destination if required
+	rm "$inputFileOrFolder" > /dev/null;
+	# create link
+	ln -s "${userDataPath}/${targetName}" "$inputFileOrFolder" || exit 1;
+}
+
+
 # Given a path to either a file or folder existing within the game's installation directory, this creates a new empty file or folder
 # (depending on what the original was) in the userdata folder, deletes the original and creates a link to the newly created file/folder
 # to where the original was.
