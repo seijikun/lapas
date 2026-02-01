afa2cb8174bfea026fc6e0cf2c05a40742634ad1
Markus Ebner <hiwatari.seiji@gmail.com>
2026-02-01 05:27:44 +0100

    umu: Fix broken userdata symlink helper method

--- res/lapas/guest/mnt/homeBase/.umuManager/hookHelpers/userdata.py
+++ res/lapas/guest/mnt/homeBase/.umuManager/hookHelpers/userdata.py
@@ -1,6 +1,8 @@
 import os;
 import string;
 import shutil;
+from enum import Enum
+
 
 def applyTemplate(srcPath, dstPath, argMap):
 	with open(srcPath, 'r') as file:
@@ -12,34 +14,51 @@ def applyTemplate(srcPath, dstPath, argMap):
 		with open(dstPath, 'w') as file:
 			file.write(data);
 
-def newEmptyLink(srcPath, dstPath):
-	"""Given a path to either a file or folder existing within the game's installation directory, this creates a new empty file or folder
-		(depending on what the original was) in the userdata folder, deletes the original and creates a link to the newly created file/folder
-		to where the original was.
-		Usage: helperUserdataNewEmptyLink <inputFileOrFolder> <relativeUserDataPath>
-		- srcPath path to the file or folder that should be replaced by a empty file/folder linked to userdata
-		- dstPath path where the file/folder should be linked from"""
-	username = os.environ['USER'];
-	
+
+class SymlinkStrategy(Enum):
+	"""
+	Move: Move the existing source to the given destination path, then symlink back
+	Empty: Start empty (empty file, empty folder), depending on what the src points to
+		If src is a file, create an empty file at the given destination path, delete original src, symlink back.
+		If src is a folder, create an empty folder at the given destination path, delete original src, symlink back
+	"""
+	Move = "Move"
+	Empty = "Empty"
+
+def setupSymlink(ctxArgs, srcPath, relDstPath, strategy = SymlinkStrategy.Move):
+	"""
+	This sets up a symlink relationship between a srcPath (somewhere in the wineprefix)
+	and the dstPath (to store the actual data in the userdata folder).
+	If dstPath already exists, this is a No-Op.
+	Arguments:
+	- ctxArgs is the entire arguments context of a umgrHook.
+	- srcPath: Path inside the wineprefix whose user-specific content should symlinked out for storage
+	- relDstPath: Destination path (relative to the userdata folder) where the data should be symlinked to.
+	- strategy: Setup strategy to use
+	"""
+	dstPath = os.path.join(ctxArgs.USERDATA_PATH, relDstPath);
+
+	if not os.path.exists(srcPath):
+		raise FileNotFoundError("Source path does not exist");
+	# initial setup already done - nothing to do
 	if os.path.islink(srcPath):
-		return; # we are done
-	    
-	# Ensure the userdata directory exists
-	os.makedirs(user_data_path, exist_ok=True);
-        
-	# Check if it's a file or directory and handle accordingly
-	if os.path.isfile(srcPath):
-		# Create an empty file in the userdata directory
-		open(dstPath, 'w').close();
-		# Remove the original file
-		os.remove(srcPath);
-	else:
-		# Create an empty folder in the userdata directory
-		os.makedirs(dstPath, exist_ok=True);
-		# Remove the original folder
-		shutil.rmtree(srcPath);
-		
-	relDstPath = os.path.relpath(dstPath, srcPath);
-	print(relDstPath);
-	raise Exception("LOL");
-	os.symlink(relDstPath, srcPath);
+		return;
+
+	# Ensure parent directory exists
+	os.makedirs(os.path.dirname(dstPath), exist_ok=True);
+
+	if strategy is SymlinkStrategy.Move:
+		# Move the source (either file or folder) to the destination, then create the symlink back.
+		shutil.move(srcPath, dstPath);
+		os.symlink(dstPath, srcPath);
+	if strategy is SymlinkStrategy.Empty:
+		# Create new empty file/folder in destination, delete original, then create the symlink back
+		if os.path.isdir(srcPath):
+			# Create an empty directory at destination and delete original
+			os.makedirs(dstPath, exist_ok=True);
+			shutil.rmtree(srcPath);
+		else:
+			# Create an empty file at destination and delete original
+			open(dstPath, "a").close();
+			os.remove(srcPath);
+		os.symlink(dstPath, srcPath);
