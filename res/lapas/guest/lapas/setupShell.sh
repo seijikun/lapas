466fa5f5b7ebd274ee9423f47be20fc13c035217
Markus Ebner <hiwatari.seiji@gmail.com>
2026-02-27 02:51:45 +0100

    Fix PS1 prefix in guest

--- res/lapas/guest/lapas/setupShell.sh
+++ res/lapas/guest/lapas/setupShell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f "/.lapasUser" ]; then
-        export PS1="USER $PS1";
+	export PS1="\[\e[1;37m\]USER\[\e[0m\] \u@\h:\w$ ";
 else
-        export PS1="ADMIN $PS1";
+	export PS1="\[\e[1;37m\]ADMIN\[\e[0m\] \u@\h:\w$ ";
 fi
